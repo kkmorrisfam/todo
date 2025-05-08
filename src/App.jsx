1ab42b40a919bd715e4de3e34697a8c9994593b1
kkmorrisfam <kkmorrisfam@hotmail.com>
2025-05-07 19:27:46 -0700

    add useEffect for local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./styles.css"
 import NewTodoForm from "./NewTodoForm"
 import { TodoList } from "./TodoList"
 
 export default function App() {
   
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(()=>{
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
 
+    return JSON.parse(localValue)
+  })
+
+  //save new values into local storage
+  useEffect(()=> {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
   function addTodo(title) {
     setTodos((currentTodos)=> {
       return [
